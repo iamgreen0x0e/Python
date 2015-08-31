75b9596c748874d4c27794eed7ec4cdbcf42b92c
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-08-31 13:10:41 +0700

    complete proxy from proxynova

--- proxy/green_getProxy.py
+++ proxy/green_getProxy.py
@@ -8,22 +8,27 @@ def readContent(url):
 	except:
 		return False
 
-
-
-def proxyNova():
-	url = 'http://www.proxynova.com/proxy-server-list/country-cn/'
-	content = readContent(url)
-	if content == False :
-		return False
-	return content
-
 def writeToFile(filename,content):
 	f = open(filename, 'a')
 	for text in content:
 		f.writelines(text+'\n\r')
 	f.close()
 
-def parseTable(text):
+def proxyNova():
+	country = ['cn','ve','tw','in','br','id','us','th','ru','ar','hk','de','eg','jp','cl','ma','co','vn','bd','ua','gb','pl','my','fr']
+	for count in country:
+		writeToFile("listProxy.txt",parseTableProxyNova(proxyNovaCountry(count)))
+		print "done in "+count
+def proxyNovaCountry(country):
+	url = 'http://www.proxynova.com/proxy-server-list/country-'+ country+'/'
+	content = readContent(url)
+	if content == False :
+		return False
+	return content
+
+
+
+def parseTableProxyNova(text):
 	data = []
 	soup = BeautifulSoup(''.join(text))
 	table = soup.find("table",{"class":"table"})
@@ -31,9 +36,13 @@ def parseTable(text):
 	for row in table_body.findAll("tr"):
 	    cols = row.findAll("td")
 	    try:
-	    	data.append("http://"+str(cols[0].find("span").contents[0])+":"+ str(cols[1].find("a").contents[0]))
-	    except:
-	    	continue
+	    	proxy = "http://"+str(cols[0].find("span").contents[0])+":"+ str(cols[1].find("a").contents[0])
+	    except AttributeError:
+	    	try:
+	    		proxy = "http://"+str(cols[0].find("span").contents[0])+":"+ str(int(cols[1].contents[0]))
+	    	except:
+	    		continue
+	    data.append(proxy)
 	return data
 
-writeToFile("proxylist1.txt",parseTable(proxyNova()))
+proxyNova()

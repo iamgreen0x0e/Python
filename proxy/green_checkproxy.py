5de752439b7be34ac843e962f06f69c3d30b124a
nguyentuananh1993 <nguyentuananh1993@gmail.com>
2015-09-01 08:19:28 +0700

    add xroxy crawler

--- proxy/green_checkproxy.py
+++ proxy/green_checkproxy.py
@@ -1,15 +1,37 @@
 import urllib2
 import os
-import BeautifulSoup import BeautifulSoup
 import re
-def check_proxy(proxy,url):
-	proxyHandle = urllib2.ProxyHandler({'http':proxy})
-	opener = urllib2.build_opener(proxyHandle)
-	try:
-		page = opener.open(url,timeout = 5)
-	except:
-		return False
-	return True
+
+
+def writeToFile(filename,content):
+	f = open(filename, 'a')
+	for text in content:
+		f.writelines(text+'\n\r')
+	f.close()
+
+def writeStringToFile(filename,string):
+	f = open(filename,'a')
+	f.writelines(text+'\n\r')
+	f.close()
+
+def check_proxy(proxyList,url):
+	# okList = []
+	for proxy in proxyList:
+		
+		try:
+			proxyHandle = urllib2.ProxyHandler({'http':proxy})
+			opener = urllib2.build_opener(proxyHandle)
+			page = opener.open(url,timeout = 5)
+			# writeStringToFile('listAliveProxy.txt', proxy)
+			print "checking "+ proxy +" is alive"
+		except KeyboardInterrupt:
+			print "thank for you using!"
+			break
+		# except:
+		# 	print "checking "+ proxy +" is dead"
+		# 	continue
+	# 	okList.append(proxy)
+	# writeToFile('listAliveProxy.txt', okList)
 
 
 # print check_proxy('http://123.30.211.95:3128','http://www.something.com/')
@@ -18,8 +40,7 @@ def check_proxy(proxy,url):
 def getProxyList(filename):
 	with open(filename) as f:
 		lines = f.readlines()
-	for line in lines:
-		line = line.replace('\n', '')
-	print lines
+	lines = [word.strip() for word in lines]
+	return lines
 
-getProxyList('proxyList.txt')
+check_proxy(getProxyList("listProxy.txt"),'http://www.something.com/')
--- proxy/green_getProxy.py
+++ proxy/green_getProxy.py
@@ -1,12 +1,16 @@
 import urllib2
 from HTMLParser import HTMLParser
 from BeautifulSoup import BeautifulSoup
+
+
 def readContent(url):
-	response = urllib2.urlopen(url)
+	req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+	response = urllib2.urlopen( req )
 	try:
 		return response.read()
 	except:
 		return False
+
 
 def writeToFile(filename,content):
 	f = open(filename, 'a')
@@ -14,18 +18,20 @@ def writeToFile(filename,content):
 		f.writelines(text+'\n\r')
 	f.close()
 
+
 def proxyNova():
 	country = ['cn','ve','tw','in','br','id','us','th','ru','ar','hk','de','eg','jp','cl','ma','co','vn','bd','ua','gb','pl','my','fr']
 	for count in country:
 		writeToFile("listProxy.txt",parseTableProxyNova(proxyNovaCountry(count)))
 		print "done in "+count
+
+
 def proxyNovaCountry(country):
 	url = 'http://www.proxynova.com/proxy-server-list/country-'+ country+'/'
 	content = readContent(url)
 	if content == False :
 		return False
 	return content
-
 
 
 def parseTableProxyNova(text):
@@ -44,5 +50,45 @@ def parseTableProxyNova(text):
 	    		continue
 	    data.append(proxy)
 	return data
+
+# def getIdXroxy():
+# 	req = urllib2.Request("http://www.xroxy.com/proxylist.php?country=IL", headers={'User-Agent' : "Magic Browser"}) 
+# 	con = urllib2.urlopen( req )
+# 	html = con.read()
+# 	# print html
+# 	data = ''
+# 	soup = BeautifulSoup(html)
+# 	content = soup.findAll("select",{"id":"country_id"})
+# 	for option in content[0].findAll("option"):
+# 		data +='\''+option['value']+'\','
+# 	return data
+
+
+def XroxyCountry(country):
+	url = "http://www.xroxy.com/proxylist.php?country="+country
+	content = readContent(url)
+	if content == False:
+		return False
+	return content
+
+def parseTableXroxy(text):
+
+	data =[]
+	soup = BeautifulSoup(text)
+	table = soup.findAll("table")
+	for counter in range(0,2):
+		rows=table[4].findAll("tr",{"class":"row"+str(counter)})
+		for row in rows:
+			column = row.findAll("td")
+			data.append("http://"+column[1].find("a").contents[0].strip()+":"+column[2].find("a").contents[0])
+	return data	
+
+
+
+def Xroxy():
+	proxyCountryList = ['AF','AL','DZ','AR','AU','AT','BD','BE','BO','BW','BR','BG','KH','CA','CL','CN','CO','CZ','DK','DO','EC','EG','EE','FR','GE','DE','GH','GB','GR','HK','IN','ID','IR','IQ','IE','IL','IT','JP','KZ','KE','KR','LV','MG','MY','MV','MU','MX','MD','MN','NP','NL','NG','PK','PA','PY','PE','PH','PL','PT','PR','RO','RU','SN','SC','SG','SK','SO','ZA','ES','LK','SE','CH','TW','TJ','TH','TN','TR','UA','US','VE','VN','ZW']
+	for count in proxyCountryList:
+		writeToFile("listProxy.txt",parseTableXroxy(XroxyCountry(count)))
+		print "done in "+count
 
 proxyNova()
